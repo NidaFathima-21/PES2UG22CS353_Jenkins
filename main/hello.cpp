4fb6c7730055037d585cf1f2ebfe23e181ea43c5
nidafathi <147266260+NidaFathima-21@users.noreply.github.com>
2025-03-13 20:23:29 +0530

    Updated hello.cpp for webhook testing

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello, git\n";
+  cout << "Hello, Webhook\n";
   cout << "I am Nida Fathima(pes2ug22cs353) and have successfully built and run this  \n";
   return 0;
 }
